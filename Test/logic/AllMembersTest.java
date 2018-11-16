7d8c2ac430032c211f4062b8e977f29e03ef55a4
Claus <Claus@10.50.131.101>
2018-11-16 15:38:18 +0100

    opret

---
+++ Test/logic/AllMembersTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Claus
+ */
+public class AllMembersTest {
+    
+    public AllMembersTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of createMember method, of class AllMembers.
+     */
+    @Test
+    public void testCreateMember() {
+        System.out.println("createMember");
+        String name = "";
+        int age = 0;
+        AllMembers instance = new AllMembers();
+        Member expResult = null;
+        Member result = instance.createMember(name, age);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of addMember method, of class AllMembers.
+     */
+    @Test
+    public void testAddMember() {
+        System.out.println("addMember");
+        Member member = null;
+        AllMembers instance = new AllMembers();
+        instance.addMember(member);
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of showAllMembers method, of class AllMembers.
+     */
+    @Test
+    public void testShowAllMembers() {
+        System.out.println("showAllMembers");
+        AllMembers instance = new AllMembers();
+        List<Member> expResult = null;
+        List<Member> result = instance.showAllMembers();
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+    
+}
