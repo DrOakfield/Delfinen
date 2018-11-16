892f8541af14fae5b05bbdff4e56ddc6e5abc107
Mads-U <Bruger@10.50.130.105>
2018-11-16 09:29:32 +0100

    Rettelse af "Members"

--- src/logic/Members.java
+++ src/logic/Members.java
@@ -1,24 +1,15 @@
 package logic;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class Members {
     private String Name;
     private int Age;
     //private int ID;
     private ArrayList<Members> allMembers = new ArrayList<>();
-    private static final boolean DEBUG = true;
-    private String MemberList = "C:\\Users\\Bruger\\Documents\\NetBeansProjects\\Delfinen\\src\\delfinen.data\\MemberList";
-                                
+
+    
     public Members(String Name, int Age/*, int ID*/) {
         this.Name = Name;
         this.Age = Age;
@@ -65,47 +56,4 @@ public class Members {
         return allMembers;
     }
     
-    public void write(List<List> ListOfMembers) {
-        ObjectOutputStream out = null;
-        try {
-            File file = new File(MemberList);
-            out = new ObjectOutputStream(new FileOutputStream(file));
-            out.writeObject(ListOfMembers);
-            out.flush();
-        } catch (FileNotFoundException ex) {
-            if(DEBUG) ex.printStackTrace();{
-            System.out.println("prøv igen senere");
-        }
-        } catch (IOException ex) {
-            if(DEBUG) ex.printStackTrace();{
-            System.out.println("prøv igen senere");
-        }
-        }
-    }
-    
-    public List<List> readObject(){
-        ObjectInputStream in = null;
-        try {
-            File file = new File(MemberList);
-            in = new ObjectInputStream(new FileInputStream(file));
-            List<List> p = (List<List>) in.readObject();
-            for (List list : p) {
-                System.out.println(list);
-            }
-            return p;
-        } catch (FileNotFoundException ex) {
-            if(DEBUG) ex.printStackTrace();{
-            System.out.println("prøv igen senere");
-        }
-        } catch (IOException ex) {
-            if(DEBUG) ex.printStackTrace();{
-            System.out.println("prøv igen senere");
-        }
-        } catch (ClassNotFoundException ex) {
-            if(DEBUG) ex.printStackTrace();{
-            System.out.println("prøv igen senere");
-        }
-        }
-        return null;
-    }
 }
