eff43498840f33a03c764f92dfb5c5ee102869a1
Mads-U <Bruger@10.50.130.105>
2018-11-20 11:56:55 +0100

    Formatteret klasse

--- src/logic/AllMembers.java
+++ src/logic/AllMembers.java
@@ -4,23 +4,27 @@ import delfinen.data.ReadWrite;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AllMembers{
+public class AllMembers {
+
     private static List<Member> allMembers = new ArrayList<>();
+
     static {
         allMembers = new ReadWrite().readObject();
-    } 
-    
-    public Member createMember(String name, int age,int Id){
+    }
+
+    public Member createMember(String name, int age, int Id) {
         return new Member(name, age, Id);
     }
-    public void addMember(Member member){
+
+    public void addMember(Member member) {
         allMembers.add(member);
         ReadWrite rw = new ReadWrite();
         rw.write(allMembers);
     }
-    public List<Member> showAllMembers(){
+
+    public List<Member> showAllMembers() {
 
         return allMembers;
-    }   
+    }
     
 }
