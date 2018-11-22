018aa05324be8b41a57b1fff120e8d2ebf8382b9
Mads-U <Bruger@10.50.130.105>
2018-11-22 16:07:49 +0100

    Tilføjet ReadWrite metoder til id, samt beskrivelser af metoder

--- src/logic/AllMembers.java
+++ src/logic/AllMembers.java
@@ -9,16 +9,33 @@ public class AllMembers {
 
     private static List<Member> allMembers = new ArrayList<>();
     private static List<Member> MedlemsRestance = new ArrayList<>();
-
+    
+    /**
+     * Copies objects from file into an arraylist
+     */
     static {
         allMembers = new ReadWrite().readObject();
 //        MedlemsRestance = new ReadWrite().readObjectMRestance();
     }
-
+    
+    /**
+     * 
+     * @param name
+     * @param age
+     * @param Id
+     * @param aktiv - aktiv/passiv boolean
+     * @param aktivTekst - aktiv/passiv tekst
+     * @param jES - Junior eller senior
+     * @param konSvøm - KonkurrenceSvømmer
+     * @return 
+     */
     public Member createMember(String name, int age, int Id, boolean aktiv, String aktivTekst, String jES, String konSvøm) {
         return new Member(name, age, Id, aktiv, aktivTekst, jES, konSvøm);
     }
-    
+    /**
+     * Adds member to arraylist (allMembers)
+     * @param member - Writes member into the text file
+     */
     public void addMember(Member member) {
         //add til medlemsoversigt
         allMembers.add(member);
@@ -27,7 +44,10 @@ public class AllMembers {
 //        MedlemsRestance.add(member);
 //        rw.writeMRestance(MedlemsRestance);
     }
-
+    /**
+     * writes all members into text file
+     * @return arraylist of all members
+     */
     public List<Member> showAllMembers() {
         rw.write(allMembers);
         return allMembers;
@@ -36,6 +56,20 @@ public class AllMembers {
     public List<Member> showRestanceOversigt(){
         rw.writeMRestance(MedlemsRestance);
         return MedlemsRestance;
+    }
+    /**
+     * reads member id's from text file
+     * @return 
+     */
+    public int readID(){
+        return rw.readWithBufferedReader();
+    }
+    /**
+     * Writes member id's to text file
+     * @param ID - member ID
+     */
+    public void writeID(int ID){
+        rw.writeWithBufferedWriter(ID);
     }
     
 }
